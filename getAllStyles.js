4d2f87845bd8adcdd7151ad17aa1ebb97f88de0b
ByDraw <heavenlywing@foxmail.com>
2017-03-26 11:34:53 +0800

    Update getAllStyles.js

--- getAllStyles.js
+++ getAllStyles.js
@@ -4,6 +4,5 @@ function getAllStyles(element) {
     for (var i = 0, len = stylesMap.length; i < len; i++) {
         styles = styles + stylesMap[i] + ":" + stylesMap.getPropertyValue(stylesMap[i]) + "<br>";
     }
-    console.log(styles);
     return styles;
 }
