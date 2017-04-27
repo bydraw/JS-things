3d7c0b5271e23385ab19de53d3ec06a4866c87f7
ByDraw <heavenlywing@foxmail.com>
2017-04-27 22:51:35 +0800

    add annotations

--- getAllStyles.js
+++ getAllStyles.js
@@ -1,14 +1,12 @@
 function getAllStyles(element) {
-    var stylesMap = element.currentStyle ? element.currentStyle : window.getComputedStyle(element, null);
-    var styles = "";
+    var stylesMap = element.currentStyle ? element.currentStyle : window.getComputedStyle(element, null);//making it compatible 
+    var styles = '';//as a String
     for (var i = 0, len = stylesMap.length; i < len; i++) {
-        styles = styles + stylesMap[i] + ":" + stylesMap.getPropertyValue(stylesMap[i]) + "<br>";
+        styles = styles + '"' + stylesMap[i] + '" : ' + '"' + stylesMap.getPropertyValue(stylesMap[i]) + '",'; //making up a String in the form of key:value
     }
-    return styles;
+    var stylesToJson = ("{" + styles.substring(0,styles.length - 1) + "}").replace(/"en"/g,"en");;//making it to JSON and specially fixing the ""en"" to "en"
+    return(JSON.parse(stylesToJson));
 }
-//will return like 
-//...
-//width:100px
-//...
-//height:100px
-//...
+//--You can use it like--
+//getAllStyles(demo).width
+//--and it will return like 100px in type of String--
