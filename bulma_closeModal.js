23f97aae23e3b769828087a215b9e2542324e0d2
ByDraw <heavenlywing@foxmail.com>
2017-04-27 19:38:37 +0800

    Update bulma_closeModal.js

--- bulma_closeModal.js
+++ bulma_closeModal.js
@@ -10,5 +10,8 @@ for (let i = 0; i < modals_deletes.length; i++) {
     modals_close.push(modals_deletes[i]);
 }
 for (let i = 0; i < modals_close.length; i++) {
-    modals_close[i].onclick = () => this.parentNode.parentNode.parentNode.classList.remove('is-active');
+    modals_close[i].onclick = removeActive;
+}
+function removeActive(){
+    this.parentNode.parentNode.parentNode.classList.remove('is-active');
 }
