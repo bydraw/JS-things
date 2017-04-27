ac8f0de2d2809b990af9ef90e9499dabd400ecdb
ByDraw <heavenlywing@foxmail.com>
2017-04-27 19:18:20 +0800

    Add files via upload

---
+++ bulma_closeModal.js
@@ -0,0 +1,16 @@
+//close modals anywhere by clicking cancel or X 
+const modals = document.getElementsByClassName('modal');
+const modals_cancels = document.querySelectorAll("a[class='button']");
+const modals_deletes = document.querySelectorAll("button[class='delete']");
+let modals_close = [];
+for (let i = 0; i < modals_cancels.length; i++) {
+    modals_close.push(modals_cancels[i]);
+}
+for (let i = 0; i < modals_deletes.length; i++) {
+    modals_close.push(modals_deletes[i]);
+}
+for (let i = 0; i < modals_close.length; i++) {
+    modals_close[i].onclick = function () {
+        this.parentNode.parentNode.parentNode.classList.remove('is-active');
+    }
+}
